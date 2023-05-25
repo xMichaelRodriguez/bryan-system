1e06e1ba43519dfeb3ca155bb3ff4fd82c0f6b1c
scottlovos503@gmail.com <xMichaelRodriguez>
2023-05-24 21:19:52 -0600

    feat: first commit

---
+++ src/api/marca/controllers/marca.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * marca controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::marca.marca');
---
+++ src/api/marca/services/marca.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * marca service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::marca.marca');
